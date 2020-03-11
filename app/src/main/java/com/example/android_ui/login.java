9684db57863d424407099fda5c281b2e56b54cc0
ssc-shimaoka <yuki_shimaoka@solver.co.jp>
2020-03-11 16:13:50 +0900

    login画面追加

---
+++ app/src/main/java/com/example/android_ui/login.java
@@ -0,0 +1,26 @@
+package com.example.android_ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+    }
+
+    void onClickloginButton()
+    {
+
+    }
+
+    void RegisterButton()
+    {
+
+    }
+}
