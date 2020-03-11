2c5bceeaea5a0365feeb9d2891bccadd232029bb
ssc-shimaoka <yuki_shimaoka@solver.co.jp>
2020-03-11 17:15:46 +0900

    Registerアクティビティ追加

---
+++ app/src/main/java/com/example/android_ui/Register.java
@@ -0,0 +1,20 @@
+package com.example.android_ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class Register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+
+    public void onClickOKButton(View view)
+    {
+
+    }
+}
--- app/src/main/java/com/example/android_ui/login.java
+++ app/src/main/java/com/example/android_ui/login.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class login extends AppCompatActivity {
 
@@ -14,13 +15,14 @@ public class login extends AppCompatActivity {
 
     }
 
-    void onClickloginButton()
+    public void onClickloginButton(View view)
     {
 
     }
 
-    void RegisterButton()
+    public void onClickRegisterButton(View view)
     {
-
+        Intent intent = new Intent(login.this, Register.class);
+        startActivity(intent);
     }
 }
